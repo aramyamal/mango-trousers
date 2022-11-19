0b5d13dce56c5df06d62d4f2f3b9ae1a381f631c
Aram Jamal <aramyamal@gmail.com>
2022-11-19 20:38:15 +0100

    solution using numpy

---
+++ Codewars solutions/4Kyu/SumOfIntervals.py
@@ -0,0 +1,11 @@
+#https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/python
+import numpy as np
+
+def merge_intervals(l):
+    l = list(zip(sorted([i[0] for i in l]), sorted([i[1] for i in l])))
+    ind = np.where(np.diff(np.array(l).flatten()) <= 0)[0]
+    ind = ind[ind % 2 == 1] #for ranges with same start as stop
+    return np.delete(l, [ind, ind+1]).reshape(-1, 2)
+
+def sum_of_intervals(intervals):
+    return sum(x[-1] - x[0] for x in merge_intervals(intervals))
