0ee4e27a6b579e909efe65e0594c25196889bd7b
Aram Jamal <aramyamal@gmail.com>
2023-01-08 15:34:56 +0100

    completed kata

---
+++ Codewars solutions/4Kyu/StripComments.py
@@ -0,0 +1,6 @@
+#https://www.codewars.com/kata/51c8e37cee245da6b40000bd/
+import re
+
+def strip_comments(strng, markers):
+    if markers == []: return strng
+    return '\n'.join([re.split(r'|'.join([ '\\' + x for x in markers]), item)[0].rstrip() for item in strng.split('\n')])
